3ec50567facb889289b5e999b2eab017c0661995
raketbizdev <radon1284@yahoo.com>
2023-05-05 21:55:33 +0800

    updated

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
-const nextConfig = {
+const path = require("path");
+
+module.exports = {
   trailingSlash: true,
   images: {
     domains: ["en.gravatar.com"],
   },
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
+  // This is required to use "next export"
+  output: {
+    path: path.join(__dirname, "out"),
+    publicPath: "/",
+  },
 };
-
-module.exports = nextConfig;
