3e2de8e0a03ea5ea86f3aed8a77192392bcb6ffa
raketbizdev <radon1284@yahoo.com>
2023-05-05 22:27:55 +0800

    update

--- next.config.js
+++ next.config.js
@@ -2,17 +2,12 @@ const path = require("path");
 
 module.exports = {
   trailingSlash: true,
-  experimental: {
-    standalone: true, // or `output: "standalone"`
-  },
-  // This is required to use "next export"
-  output: "export",
-  exportPathMap: async function () {
+  output: "standalone",
+  generateStaticParams: async function () {
     return {
       "/": { page: "/" },
     };
   },
-  // This is required to use "next export"
   output: {
     path: path.join(__dirname, "out"),
     publicPath: "/",
