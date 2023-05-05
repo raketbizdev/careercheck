6bd62b726f5b67d6145bb5fe03d12805bc9d694d
raketbizdev <radon1284@yahoo.com>
2023-05-05 21:49:32 +0800

    updated

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   images: {
     domains: ["en.gravatar.com"],
-  },
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" },
-    };
-  },
-  exportTrailingSlash: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-    };
   },
 };
 
