ce931ba0e590916cfa7c9e4638b8a05b0f8f0000
raketbizdev <radon1284@yahoo.com>
2023-05-06 12:32:53 +0800

    updating

--- src/components/MetaHead.js
+++ src/components/MetaHead.js
@@ -3,36 +3,36 @@ import Script from "next/script";
 
 const MetaHead = ({ title, description, image, favicon, baseUrl, ga4 }) => {
   return (
-    <>
-      <Head>
-        <title>CareerCheck</title>
-        <link rel="icon" href={favicon} />
-        <title>{title}</title>
-        <meta name="title" content={title} />
-        <meta name="description" content={description} />
+    <Head>
+      <title>CareerCheck</title>
+      <link rel="icon" href={favicon} />
+      <title>{title}</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
 
-        {/* Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={baseUrl} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={`${baseUrl}${image}`} />
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={baseUrl} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={`${baseUrl}${image}`} />
 
-        {/* Twitter */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content={baseUrl} />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={description} />
-        <meta property="twitter:image" content={`${baseUrl}${image}`} />
-      </Head>
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={baseUrl} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={`${baseUrl}${image}`} />
 
       {/* Google Analytics 4 */}
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${ga4}`}
         strategy="afterInteractive"
+        id="ga4-script"
       />
       <Script
         strategy="afterInteractive"
+        id="ga4-setup-script"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
@@ -42,7 +42,7 @@ const MetaHead = ({ title, description, image, favicon, baseUrl, ga4 }) => {
           `,
         }}
       />
-    </>
+    </Head>
   );
 };
 
